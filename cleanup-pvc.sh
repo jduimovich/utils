f841e37911b6db6fa5728dbb48d23cdf86cf51ad
John Duimovich <jduimovich@gmail.com>
2021-11-30 19:36:15 -0500

    hackery

---
+++ cleanup-pvc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+for dir in *pv-*/ ; do
+    buildDir=${dir%/}
+    echo "Checking for PipelineRun $buildDir"
+    if  oc get pr "$buildDir" 2>/dev/null >/dev/null; then
+        echo PR "$buildDir exists, leave PVC" 
+    else
+        echo PR "$buildDir does not exist, rm  PVC" 
+        rm -rf $buildDir
+    fi 
+done
+  
+   
