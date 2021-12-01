2ce635350d8f5472ce07f388a9ee5e7a07734c15
John Duimovich <jduimovich@gmail.com>
2021-11-30 19:50:33 -0500

    clean

--- cleanup-pvc.sh
+++ cleanup-pvc.sh
@@ -2,6 +2,10 @@
 for dir in * ; do 
     if [ -d "$dir" ]; then    
         echo "Checking for PipelineRun $dir"
+        echo "getall "
+        oc get pr  
+        echo "one "
+        oc get pr "$dir" 
         if  oc get pr "$dir" 2>/dev/null >/dev/null; then
             echo PR "$dir exists, leave PVC" 
         else
