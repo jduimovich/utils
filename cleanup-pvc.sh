d019bb0f2389449a44fedd58e8023d3cf9e332cb
John Duimovich <jduimovich@gmail.com>
2021-11-30 19:43:59 -0500

    "pv-*/"

--- cleanup-pvc.sh
+++ cleanup-pvc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
-for dir in *pv-*/ ; do
+for dir in "pv-*/" ; do
     buildDir=${dir%/}
+    echo "Dir is $dir"
     echo "Checking for PipelineRun $buildDir"
     if  oc get pr "$buildDir" 2>/dev/null >/dev/null; then
         echo PR "$buildDir exists, leave PVC" 
